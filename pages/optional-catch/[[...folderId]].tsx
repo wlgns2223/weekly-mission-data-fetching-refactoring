1abbd714249f567629e5c2730c13e40a05f6e50b
ShimJiHoon <wlgns2223@gmail.com>
2024-04-05 21:51:18 +0900

    getStaticPaths

--- pages/optional-catch/[[...folderId]].tsx
+++ pages/optional-catch/[[...folderId]].tsx
@@ -1,37 +1,55 @@
-import { GetServerSideProps, GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import FolderPage, { Folder, FolderPageProps, Link } from "../../src/folder";
 import { axiosInstance } from "../../src/util/axiosInstance";
+import { mapFolderData } from "../../src/util/mapFolderData";
+import { mapFolderFromLink } from "../../src/util/mapFolderFromLink";
 
-// const params = {
-//     folderId: [1]
-// }
+export const getStaticPaths = (async () => {
+    const { data } = await axiosInstance.get<{ data: Folder[] }>("users/1/folders");
 
-export const getServerSideProps = (async (context) => {
-    if (context.params && context.params.folderId && context.params.folderId.length > 1) {
+    const paths = data.data
+        .map((folder) => folder.id.toString())
+        .map((m) => ({ params: { folderId: [m] } }));
+
+    // [
+    //     { params: { folderId: [ '14' ] } },
+    //     { params: { folderId: [ '24' ] } },
+    //     { params: { folderId: [ '307' ] } },
+    //     { params: { folderId: [ '433' ] } },
+    //     { params: { folderId: [ '441' ] } },
+    //     { params: { folderId: [ '457' ] } },
+    //     { params: { folderId: [ '480' ] } },
+    //     { params: { folderId: [ '513' ] } }
+    //   ]
+
+    return {
+        paths,
+        fallback: true, // false or "blocking"
+    };
+}) satisfies GetStaticPaths<any>;
+
+export const getStaticProps = (async (context) => {
+    if (context.params && context.params.folderId && context.params.folderId?.length > 1) {
         return {
             notFound: true,
         };
     }
-
-    // 전체폴더 http://localhost:3000/folder
-    // 다른 폴더 http://localhost:3000/folder/11
-
     const folderId = context.params?.folderId?.[0];
     const apiPath = !!folderId ? `users/1/links?folderId=${folderId}` : "users/1/links";
     const { data } = await axiosInstance.get<{ data: Link[] }>(apiPath);
-    const { data: folders } = await axiosInstance.get<{ data: Folder[] }>("users/1/folders");
+    const {
+        data: { data: folders },
+    } = await axiosInstance.get<{ data: Folder[] }>("users/1/folders");
 
     return {
         props: {
-            links: data.data,
-            folders: folders.data,
+            links: mapFolderFromLink(data.data).data,
+            folders,
         },
     };
-}) satisfies GetServerSideProps<any>;
+}) satisfies GetStaticProps<any>;
 
 export const OptionalCatch: NextPage<FolderPageProps> = (props) => {
-    console.log(props.links);
-
     return <FolderPage {...props} />;
 };
 
